c71e954dcbc1a119ad4ea94ec5d286707648843a
Chetan M <chetanmanagavi@gmail.com>
2023-06-12 17:21:51 +0530

    added smart contract

--- src/connect.js
+++ src/connect.js
@@ -2,230 +2,155 @@ import { Connection, SystemProgram, PublicKey } from "@solana/web3.js";
 // import { Buffer } from "buffer";
 import * as buffer from 'buffer';
 import { sendAndConfirmTransaction } from "@solana/web3.js";
-import {PhantomWalletAdapter} from "@solana/wallet-adapter-phantom"
-
-
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom"
+// import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
 
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
+// import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
 
 
 
-const idl = 
-{
-    "version": "0.1.0",
-    "name": "hello_anchor",
-    "instructions": [
-        {
-            "name": "initialize",
-            "accounts": [
-                {
-                    "name": "patientAccount",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "authority",
-                    "isMut": true,
-                    "isSigner": true
-                },
-                {
-                    "name": "systemProgram",
-                    "isMut": false,
-                    "isSigner": false
-                }
-            ],
-            "args": [
-                {
-                    "name": "name",
-                    "type": "string"
-                },
-                {
-                    "name": "pid",
-                    "type": "u32"
-                }
-            ]
-        },
-        {
-            "name": "addFile",
-            "accounts": [
-                {
-                    "name": "patientAccount",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "fileAccount",
-                    "isMut": true,
-                    "isSigner": false
-                },
-                {
-                    "name": "authority",
-                    "isMut": true,
-                    "isSigner": true
-                },
-                {
-                    "name": "systemProgram",
-                    "isMut": false,
-                    "isSigner": false
-                }
-            ],
-            "args": [
-                {
-                    "name": "cid",
-                    "type": "string"
-                }
-            ]
-        }
-    ],
-    "accounts": [
-        {
-            "name": "FileAccount",
-            "type": {
-                "kind": "struct",
-                "fields": [
-                    {
-                        "name": "cid",
-                        "type": "string"
-                    },
-                    {
-                        "name": "owner",
-                        "type": "publicKey"
-                    }
-                ]
-            }
-        },
-        {
-            "name": "PatientAccount",
-            "type": {
-                "kind": "struct",
-                "fields": [
-                    {
-                        "name": "pid",
-                        "type": "u32"
-                    },
-                    {
-                        "name": "authority",
-                        "type": "publicKey"
-                    },
-                    {
-                        "name": "name",
-                        "type": "string"
-                    },
-                    {
-                        "name": "fileCount",
-                        "type": "u16"
-                    }
-                ]
-            }
-        }
-    ]
-}
+
+
+// const idl8={"version":"0.1.0","name":"hello_anchor","instructions":[{"name":"initialize","accounts":[{"name":"patientAccount","isMut":true,"isSigner":false},{"name":"authority","isMut":true,"isSigner":true},{"name":"systemProgram","isMut":false,"isSigner":false}],"args":[{"name":"name","type":"string"},{"name":"pid","type":"u8"},{"name":"gender","type":"string"}]},{"name":"addFile","accounts":[{"name":"patientAccount","isMut":true,"isSigner":false},{"name":"fileAccount","isMut":true,"isSigner":false},{"name":"authority","isMut":true,"isSigner":true},{"name":"systemProgram","isMut":false,"isSigner":false}],"args":[{"name":"cid","type":"string"}]}],"accounts":[{"name":"FileAccount","type":{"kind":"struct","fields":[{"name":"cid","type":"string"},{"name":"owner","type":"publicKey"}]}},{"name":"PatientAccount","type":{"kind":"struct","fields":[{"name":"pid","type":"u8"},{"name":"authority","type":"publicKey"},{"name":"name","type":"string"},{"name":"gender","type":"string"},{"name":"fileCount","type":"u16"}]}}]}
+
+const idl11={"version":"0.1.0","name":"hello_anchor","instructions":[{"name":"initialize","accounts":[{"name":"patientAccount","isMut":true,"isSigner":false},{"name":"authority","isMut":true,"isSigner":true},{"name":"systemProgram","isMut":false,"isSigner":false}],"args":[{"name":"pid","type":"u8"},{"name":"name","type":"string"},{"name":"dob","type":"string"},{"name":"bloodgroup","type":"string"},{"name":"gender","type":"string"},{"name":"origin","type":"string"}]},{"name":"addFile","accounts":[{"name":"patientAccount","isMut":true,"isSigner":false},{"name":"fileAccount","isMut":true,"isSigner":false},{"name":"authority","isMut":true,"isSigner":true},{"name":"systemProgram","isMut":false,"isSigner":false}],"args":[{"name":"pid","type":"u8"},{"name":"cid","type":"string"}]}],"accounts":[{"name":"FileAccount","type":{"kind":"struct","fields":[{"name":"cid","type":"string"},{"name":"owner","type":"publicKey"}]}},{"name":"PatientAccount","type":{"kind":"struct","fields":[{"name":"pid","type":"u8"},{"name":"authority","type":"publicKey"},{"name":"name","type":"string"},{"name":"dob","type":"string"},{"name":"bloodgroup","type":"string"},{"name":"gender","type":"string"},{"name":"origin","type":"string"},{"name":"fileCount","type":"u8"}]}}]}
 
 console.log("This is loading as page loads")
-const getProvider = () => {
-  if ('phantom' in window) {
-    const provider = window.phantom?.solana;
-    window.Buffer = buffer.Buffer;
+// const getProvider = () => {
+//     if ('phantom' in window) {
+//         const provider = window.phantom?.solana;
+//         window.Buffer = buffer.Buffer;
 
-    if (provider?.isPhantom) {
-      return provider;
-    }
-  }
+//         if (provider?.isPhantom) {
+//             return provider;
+//         }
+//     }
 
 
-  window.open('https://phantom.app/', '_blank');
-};
+//     window.open('https://phantom.app/', '_blank');
+// };
 
 export const getAccount = async function () {
-  console.log('clicked')
-
-//  const provider = getProvider();
-  try {
-    const walletAdapter = new PhantomWalletAdapter();
-    await walletAdapter.connect()
-    window.Buffer = buffer.Buffer;
-    console.log("wallet pubkey",walletAdapter.publicKey.toString())
-    const programkey = new PublicKey("6pK4Wk9BVdsWm86aefUH9TYcG3ATXKkqTVGpKtioxbuo")
-    const conn = new Connection("https://api.devnet.solana.com")
-
-    const provider=new anchor.AnchorProvider(conn, walletAdapter, anchor.AnchorProvider.defaultOptions());
-    const program = new anchor.Program(idl, programkey, provider)
-    // await walletAdapter.connect();
-    // console.log("pub",walletAdapter.publicKey)
-  //  const resp = await provider.connect();
-    // console.log(resp.publicKey.toString());
-
-
-    // console.log(conn)
-    // console.log(idl)
-
-
-
-    // //smart contract
-    // console.log("Smart Contract ", program)
-    console.log("program id", program.programId.toString())
-   
-    const publicKeyBuffer = walletAdapter.publicKey.toBuffer();
-
-
-    
-    // //for testing purpose .
-    // //solana ide waller public key
-    const testpublickey = new PublicKey("C73cUPMXP9Z6WCsnhhSwxooNHyraiusJZP7foevEqQv");
-    const testpublickeyBuffer = testpublickey.toBuffer();
-    // console.log("test public key buffer", testpublickeyBuffer)
-
-    // //get Patient Account address
-    const [userPda] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("new-patient"), publicKeyBuffer], program.programId)
-    console.log("userpda", userPda.toString())
-
-    // const userinfo=await program.account.patientAccount.fetch(userPda)
-    // console.log("userinfo", userinfo)
-
-    // const [filePda] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("patient-file"), testpublickeyBuffer], program.programId)
-    // //trying to fetch patient account
-    // console.log("filepda", filePda.toString())
-    // try {
-    // const userinfo = await conn.getAccountInfo(userPda)
-    //     console.log("userinfo", userinfo)
-    //     // alert(userinfo);
-        const trans=await program.methods
-          .initialize("abcdefghi",2)
-          .accounts({
-            patientAccount: userPda,
-            //fileAccount:filePda,
-            authority: walletAdapter.publicKey,
-            systemProgram: SystemProgram.programId,
-          })
-          .rpc()
-          console.log(trans)
-
-          
-
-    
-        
-    // }
-    // catch (err) {
-    //     console.log(err)
-    //   alert(err);
-    // }
+    console.log('clicked')
+ 
     
 
+    try {
+        const walletAdapter = new PhantomWalletAdapter();
+        await walletAdapter.connect()
+        window.Buffer = buffer.Buffer;
+        console.log("wallet pubkey", walletAdapter.publicKey.toString())
 
+        //to be changed when new program is deployed
+        const programkey = new PublicKey("5WZhfHHh64Z2GrkVpBh7rGfNy7nkPgsiJaTTr3Qu24eL")
+        const conn = new Connection("https://api.devnet.solana.com")
 
+      const provider = new anchor.AnchorProvider(conn, walletAdapter, anchor.AnchorProvider.defaultOptions());
+       
+       //to be changed when new program is built
+       //should be avilable globally
+        const  program = new anchor.Program(idl11, programkey, provider)
+        console.log("program id", program.programId.toString())
 
-
+    } catch (err) {
+        console.log(err)
+        alert(err);
     
-
-  } catch (err) {
-    console.log(err)
-    alert(err);
-
-  }
-
-
+    }
 }
 
 
+
+
+       export const createPatient = async function () {
+        const walletAdapter = new PhantomWalletAdapter();
+        await walletAdapter.connect()
+        window.Buffer = buffer.Buffer;
+        console.log("wallet pubkey", walletAdapter.publicKey.toString())
+
+        //to be changed when new program is deployed
+        const programkey = new PublicKey("5WZhfHHh64Z2GrkVpBh7rGfNy7nkPgsiJaTTr3Qu24eL")
+        const conn = new Connection("https://api.devnet.solana.com")
+
+      const provider = new anchor.AnchorProvider(conn, walletAdapter, anchor.AnchorProvider.defaultOptions());
+       
+       //to be changed when new program is built
+       //should be avilable globally
+        const program = new anchor.Program(idl11, programkey, provider)
+        console.log("program id", program.programId.toString())
+       
+        const publicKeyBuffer = walletAdapter.publicKey.toBuffer();       
+            
+        //functio to create patient account
+        //generate user pubkey
+        const [userPda] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("new-patient"), publicKeyBuffer,Uint8Array.from([7])], program.programId)
+       
+        const patientcreated = await program.methods
+            .initialize( 7,"patientname1","27/06/2002","A+ve","female","tumkur")    //arguments to be passed for patient account
+            .accounts({
+                patientAccount: userPda,
+                authority: walletAdapter.publicKey,
+                systemProgram: SystemProgram.programId,
+            })
+            .rpc()
+            console.log(patientcreated)
+        }
+
+
+
+
+
+       export const AddFile = async function () {
+        const walletAdapter = new PhantomWalletAdapter();
+        await walletAdapter.connect()
+        window.Buffer = buffer.Buffer;
+        console.log("wallet pubkey", walletAdapter.publicKey.toString())
+
+        //to be changed when new program is deployed
+        const programkey = new PublicKey("5WZhfHHh64Z2GrkVpBh7rGfNy7nkPgsiJaTTr3Qu24eL")
+        const conn = new Connection("https://api.devnet.solana.com")
+
+      const provider = new anchor.AnchorProvider(conn, walletAdapter, anchor.AnchorProvider.defaultOptions());
+       
+       //to be changed when new program is built
+       //should be avilable globally
+         const program = new anchor.Program(idl11, programkey, provider)
+        console.log("program id", program.programId.toString())
+       
+        const publicKeyBuffer = walletAdapter.publicKey.toBuffer();       
+            
+        //functio to create patient account
+        //generate user pubkey
+        const [userPda] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("new-patient"), publicKeyBuffer,Uint8Array.from([6])], program.programId)
+        const [filePda] = anchor.web3.PublicKey.findProgramAddressSync([Buffer.from("patient-file"),userPda.toBuffer(),Uint8Array.from([0]) ], program.programId)
+        const filecreated= await program.methods
+              .addFile(4,"cid1")  //arguementsto smartcontract method for adding file
+              .accounts({
+                patientAccount: userPda,
+                fileAccount: filePda,
+                authority: walletAdapter.publicKey,
+                systemProgram: SystemProgram.programId,
+              }).rpc()
+
+        console.log(filecreated)
+       
+        }
+
+
+            
+        
+        
+
+        
+
+        
+
+
+
+
+
+
+  
 
--- src/lib/newReg.svelte
+++ src/lib/newReg.svelte
@@ -1,5 +1,17 @@
 <script>
 	import { Input, Label, Helper, Button, Checkbox, A, Select } from 'flowbite-svelte';
+	import {createPatient} from "/src/connect.js"
+// function createData() {
+// Pdata = {
+// 	pid =1
+// 	name : name,
+// 	dob : dob,
+// 	bloodgroup : bloodgroup,
+// 	origin:origin
+
+// };
+// }
+
 	let gender_items = [
 		{ value: 'M', name: 'MALE' },
 		{ value: 'F', name: 'FEMALE' },
@@ -22,21 +34,23 @@
 	];
 
 	let mstatus_selected, gender_selected, blood_selected;
+	let name , dob, bloodgroup ,phonenum ,mart_status,emal_addr,gender,origin;
+	
 </script>
 
 <form>
 	<div class="grid gap-6 mb-6 md:grid-cols-2">
 		<div>
 			<Label for="first_name" class="mb-2">First name</Label>
-			<Input type="text" id="first_name" placeholder="John" required />
+			<Input type="text" id="first_name" placeholder="John" bind:value={name}   />
 		</div>
 		<div>
 			<Label for="last_name" class="mb-2">Last name</Label>
-			<Input type="text" id="last_name" placeholder="Doe" required />
+			<Input type="text" id="last_name" placeholder="Doe"   />
 		</div>
 		<div>
 			<Label for="company" class="mb-2">DOB</Label>
-			<Input type="text" id="company" placeholder="DD/MM/YYY" required />
+			<Input type="text" id="company" placeholder="DD/MM/YYY"   />
 		</div>
 		<div>
 			<Label for="bloodgroup" class="mb-2">Blood group</Label>
@@ -49,8 +63,9 @@
 					type="tel"
 					id="phone"
 					placeholder="123-45-678"
-					pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-					required
+					
+					 
+					
 				/>
 			</div>
 		</div>
@@ -60,7 +75,7 @@
 		</div>
 		<div class="mb-6">
 			<Label for="email" class="mb-2">Email address</Label>
-			<Input type="email" id="email" placeholder="john.doe@company.com" required />
+			<Input type="email" id="email" placeholder="john.doe@company.com"   />
 		</div>
 		<div class="mb-6">
 			<Label for="gender" class="mb-2">Gender</Label>
@@ -68,12 +83,12 @@
 		</div>
 		<div>
 			<Label for="origin" class="mb-2">Origin</Label>
-			<Input type="text" id="origin" placeholder="State your origin" required />
+			<Input type="text" id="origin" placeholder="State your origin"   />
 		</div>
 
-		<Checkbox class="mb-6 space-x-1" required
+		<Checkbox class="mb-6 space-x-1"  
 			>I agree with the <A href="/">terms and conditions</A>.</Checkbox
 		>
-		<Button type="submit" color="primary">Submit</Button>
+		<Button type="submit" color="primary" on:click={()=>createPatient()}>Submit</Button>
 	</div>
 </form>
